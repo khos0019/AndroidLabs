057c0377d9d49978cfe9b955f2d19e44799d7cdb
khos0019 <khos0019@algonquinlive.com>
2020-06-03 17:21:58 -0400

    step 1

--- app/src/main/java/com/example/andriodlabstest/MainActivity.java
+++ app/src/main/java/com/example/andriodlabstest/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_grid);
+        setContentView(R.layout.activity_main_linear);
 
         button = findViewById(R.id.Button);
         button.setOnClickListener(this);
