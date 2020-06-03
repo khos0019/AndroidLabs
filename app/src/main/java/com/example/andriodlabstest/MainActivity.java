74a89e3bbe0b56f540bb22ccb673cb273478a2b7
khos0019 <khos0019@algonquinlive.com>
2020-06-03 14:53:41 -0400

    step 1

--- app/src/main/java/com/example/andriodlabstest/MainActivity.java
+++ app/src/main/java/com/example/andriodlabstest/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -16,19 +17,19 @@ import com.google.android.material.snackbar.Snackbar;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
     private Button button;
-    private LinearLayout linearLayout;
+    private GridLayout gridLayout;
     private CheckBox checkBox;
     private Switch aSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_grid);
 
         button = findViewById(R.id.Button);
         button.setOnClickListener(this);
 
-        linearLayout = findViewById(R.id.linearLayout);
+        gridLayout = findViewById(R.id.gridLayout);
         checkBox = findViewById(R.id.checkBox);
         aSwitch = findViewById(R.id.Switch);
 
@@ -44,13 +45,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onCheckedChanged(final CompoundButton buttonView, final boolean isChecked) {
         if(isChecked){
-            Snackbar snackbar = Snackbar.make(linearLayout, getResources().getString(R.string.switch_on), Snackbar.LENGTH_SHORT)
+            Snackbar snackbar = Snackbar.make(gridLayout, getResources().getString(R.string.switch_on), Snackbar.LENGTH_LONG)
                     .setAction("UNDO", click -> buttonView.setChecked(!isChecked));
 
             snackbar.show();
 
         } else {
-            Snackbar snackbar = Snackbar.make(linearLayout, getResources().getString(R.string.switch_off), Snackbar.LENGTH_SHORT)
+            Snackbar snackbar = Snackbar.make(gridLayout, getResources().getString(R.string.switch_off), Snackbar.LENGTH_LONG)
                     .setAction("UNDO", click -> buttonView.setChecked(!isChecked));
 
             snackbar.show();
